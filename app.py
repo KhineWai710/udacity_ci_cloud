8a380c4ea4118445d281246bb199c737afe7f02e
khine wai oo <khingwai10@gmail.com>
2021-03-28 12:45:10 +0000

    added app file, docker and requirements file

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-def myfunc():
-    print("hello")
-    
-myfunc()
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
